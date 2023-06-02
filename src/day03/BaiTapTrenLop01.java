084b18f460bac1d6eecc3bea5eb1320ba6d77b56
maiquyen2403 <maiquyen2403@gmail.com>
2023-06-02 21:21:05 +0700

    update

--- src/day03/BaiTapTrenLop01.java
+++ src/day03/BaiTapTrenLop01.java
@@ -4,7 +4,7 @@ import java.util.concurrent.ThreadLocalRandom;
 
 /*Bài 1: Viết chương trình lấy 1 số nguyên bất kỳ trong khoảng từ 0 đến 1000 và tính tổng các chữ số của nó?
         VD: Tổng các chữ số của số 1000 = 1;*/
-public class BaiTapTrenLop {
+public class BaiTapTrenLop01 {
     public static void main(String[] args) {
         int random = ThreadLocalRandom.current().nextInt(1, 1001);
         int sum = calculateSum(random);
